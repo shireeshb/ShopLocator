2f90bcbdf527a665e179c4cae9c65702a3d788f4
shireeshb <shirish.bakawle@gmail.com>
2016-06-06 22:31:30 +0530

    Added Post Method

--- src/main/java/com/db/locate/location/controller/LocationController.java
+++ src/main/java/com/db/locate/location/controller/LocationController.java
@@ -3,12 +3,13 @@ package com.db.locate.location.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.db.locate.location.dto.GoogleResponse;
-import com.db.locate.location.dto.Result;
+import com.db.locate.location.address.ShopDetails;
 import com.db.locate.location.service.LocationService;
 
 
@@ -24,22 +25,14 @@ public class LocationController {
 	 * @return
 	 * @throws IOException 
 	 */
-	@RequestMapping("/location")
-	public String locatePosition(@RequestParam(value="address",defaultValue="Pune") String address) throws IOException{
-		GoogleResponse res = locationService.convertToLatLong(address);
 		
-		if (res.getStatus().equals("OK")) {
-			for (Result result : res.getResults())
-				return String.format("Address Lattitude: %s, Longitude: %s Location: %s",
-						result.getGeometry().getLocation().getLat(),
-						result.getGeometry().getLocation().getLng(), address);
-
-		} else {
-			return res.getStatus();
-		}
-		return address;
+	@RequestMapping(value="/location", method=RequestMethod.POST)
+	public String locatePosition(@RequestBody ShopDetails shopDetails) throws IOException{
+		
+		String details = locationService.convertToLatLong(shopDetails);		
+		return details;
 	}
-	
+
 	/**
 	 * This method will return the shop address
 	 * @param lat
@@ -48,20 +41,12 @@ public class LocationController {
 	 * @throws IOException
 	 */
 	@RequestMapping("/shop")
-	public String locateNearestShop(@RequestParam(value = "lat") String lat, @RequestParam(value = "lng") String lng) throws IOException {
+	public String locateNearestShop(@RequestParam(value = "customerLatitude") String lat, @RequestParam(value = "customerLongitude") String lng) throws IOException {
 
 		String latlng = String.format("%s,%s", lat,lng );
-		GoogleResponse res = locationService.convertFromLatLong(latlng);
-		if (res.getStatus().equals("OK")) {
-			for (Result result : res.getResults())
-				return String.format("Lattitude: %s, Longitude: %s Address: %s",
-						result.getGeometry().getLocation().getLat(),
-						result.getGeometry().getLocation().getLng(), result.getFormatted_address());
-
-		} else {
-			return res.getStatus();
-		}
-		return latlng;
+		String shopAddress = locationService.convertFromLatLong(latlng);
+		
+		return shopAddress;
 		
 
 	}
